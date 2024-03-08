4018058ac9cbae12d93f716e4d5cfedd861d05ab
Ernestas Damanskis <ernestasdamanskis@gmail.com>
2024-03-08 11:11:46 +0200

    Added a simple volume slider

--- menus/options_menu.gd
+++ menus/options_menu.gd
@@ -2,6 +2,14 @@ extends Control
 
 
 
+@export
+var bus_name: String
+
+var bus_index: int
+
+func _ready() -> void:
+	bus_index = AudioServer.get_bus_index(bus_name)
+		
 
 func _on_back_pressed():
 	get_tree().change_scene_to_file("res://menus/menu.tscn")
