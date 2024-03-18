78dfb3a4115efb440f8da2dbb00cdea86b30a531
Ugnius <u.stasaitis@gmail.com>
2024-03-18 14:32:28 +0200

    Mucho refactoro

--- board/boards/player_board.gd
+++ board/boards/player_board.gd
@@ -5,15 +5,15 @@ var indicator
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	board = $PlayerBoard/Board/GridTiles
-	indicator = $PlayerBoard/Board/Indicator
+	board = $Board/GridTiles
+	indicator = $Board/Indicator
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var mouse_position = 
-	var grid_coords = get_mouse_grid_position()
-	var indicator = $PlayerBoard/Indicator
+	#todo move this out
+	var mouse_position = get_viewport().get_mouse_position()
+	var grid_coords = get_mouse_grid_position(mouse_position)
 	
 	if !(grid_coords.x == -1 || grid_coords.y == -1):
 		indicator.visible = true
